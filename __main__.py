1594d3110e39fb46748b9912c2864d5c1018b318
RenoFar <renaudfaraci@gmail.com>
2021-01-21 16:07:47 +0100

    create __main__.py

---
+++ __main__.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+action = [['share-1', 15, 10], ['share-2', 25, 15], ['share-3', 35, 20], ['share-4', 30, 17],
+          ['share-5', 40, 25], ['share-6', 11, 7], ['share-7', 13, 11], ['share-8', 24, 13],
+          ['share-9', 17, 27], ['share-10', 21, 17], ['share-11', 55, 9], ['share-12', 19, 23],
+          ['share-13', 7, 1], ['share-14', 9, 3], ['share-15', 4, 8], ['share-16', 2, 12],
+          ['share-17', 5, 14], ['share-18', 12, 21], ['share-19', 57, 18], ['share-20', 10, 5]]
+
+
+def greedy_algorithm():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
